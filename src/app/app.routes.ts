a3afad4ccb0eef38c18770948f83503806e40cb0
Alexis28J <jordy2806@hotmail.com>
2026-02-16 13:30:29 +0100

    Finire il task

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Page1 } from './components/page1/page1';
+import { Page2 } from './components/page2/page2';
+import { Page3 } from './components/page3/page3';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'page1', component: Page1},
+    { path: 'page2', component: Page2},
+    { path: 'page3', component: Page3},
+    { path: '**', redirectTo: '/home', pathMatch: 'full'}
+];
